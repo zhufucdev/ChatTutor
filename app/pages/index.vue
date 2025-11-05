689b9728d03daff190a2adfbc183ea34a56c5dc1
Acbox <acbox0328@gmail.com>
2025-11-05 21:34:33 +0800

    feat(app): add welcome page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,1 +1,12 @@
-<template></template>
+<script setup lang="ts">
+const input = ref('')
+</script>
+
+<template>
+  <div class="size-full h-screen flex flex-col gap-10 items-center justify-center">
+    <h1 class="text-4xl font-mono text-gray-600">Welcome to ChatTutor!</h1>
+    <div class="w-full max-w-200 h-35">
+      <PromptArea v-model:input="input" />
+    </div>
+  </div>
+</template>
